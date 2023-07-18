c45e4403d9262e6399adb9372b969a10512cdc40
PriyankaDexcom <pagarwal.ag@gmail.com>
2023-07-18 16:41:45 -0700

    Added Submit

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -3,5 +3,6 @@ public class login {
     public static void main(String[] args) {
         System.out.println("Enter Username");
         System.out.println("Enter Password");
+        System.out.println("Click Submit");
     }
 }
