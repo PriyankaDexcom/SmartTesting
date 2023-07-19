09c670cf48666a0f705db945947f5bb0c31e0a8b
PriyankaDexcom <pagarwal.ag@gmail.com>
2023-07-18 20:54:02 -0700

    **

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,8 +1,10 @@
 public class login {
 
     public static void main(String[] args) {
+        System.out.println("************");
         System.out.println("Enter Username");
         System.out.println("Enter Password");
         System.out.println("Click Submit");
+        System.out.println("************");
     }
 }
